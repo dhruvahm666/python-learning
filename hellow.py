80374158d8882d9af47574b98d9b1706305ae262
dhruva666 <dhruvahm2009@gmail.com>
2026-05-30 20:45:55 +0530

    my first Python file

---
+++ hellow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Simple hello world script."""
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
+print("Hello! I am learning Python!")
+print("My name is Dhruva")
+print("Today is my first day of coding!")
