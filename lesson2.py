5f54b1ecc8874de5fb4e1231045443a48486a293
dhruva666 <dhruvahm2009@gmail.com>
2026-06-03 21:33:46 +0530

    lesson 2 variables complete

---
+++ lesson2.py
@@ -0,0 +1,10 @@
+name='DHRUVA'
+grade='12th'
+fovsubj='maths'
+hobby='songs,movies,anime'
+address='channapatna'
+print('my name is',name)
+print('my grade is',grade)
+print('my favourite subject is',fovsubj)
+print('my hobby is',hobby)
+print('my address is',address)
