847bb58d35651c8e4edeb8d2846bd6e07b4e036e
dhruva666 <dhruvahm2009@gmail.com>
2026-06-05 21:59:39 +0530

    lesson 6 multiple onditions

---
+++ lesson6.py
@@ -0,0 +1,10 @@
+age=input('what is your age?')
+if int(age) >= 18:
+    print('you are eligible to vote')
+else:    print('you are not eligible to vote')
+
+num=int(input('enter a number'))
+if num%2==0:
+    print('the number is even')
+else:
+    print('the number is odd')
