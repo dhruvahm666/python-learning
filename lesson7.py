df167d67e25d6bacca169738cc6bc8f99652ce50
dhruva666 <dhruvahm2009@gmail.com>
2026-06-06 23:11:50 +0530

    lesson 7 for loops complete

---
+++ lesson7.py
@@ -0,0 +1,8 @@
+print('counting numbers from 1 to 1000')
+for i in range(1, 1001):
+    print(i)
+
+num = int(input('enter a number: '))
+print('multiplying the given numbers by 1 to 1000')
+for i in range(1, 1001):
+    print(num, 'x', i, '=', num * i)
