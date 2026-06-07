85b2c5b932a30bdbf2918fc91417c462f789936c
dhruva666 <dhruvahm2009@gmail.com>
2026-06-07 20:23:34 +0530

    lesson 9 lists complete

---
+++ lesson9.py
@@ -0,0 +1,12 @@
+students = ["Dhruva", "Rahul", "Priya", "Amit"]
+
+print(students)              
+print(students[0])           
+print(students[1])           
+print(len(students))         
+
+students.append("Ravi")      
+print(students)              
+
+students.remove("Rahul")     
+print(students)
