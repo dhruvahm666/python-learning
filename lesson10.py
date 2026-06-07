825740fad928a9121e0c0fc2246b6f65bc6405b8
dhruva666 <dhruvahm2009@gmail.com>
2026-06-07 20:31:52 +0530

    lesson 10 functions complete

---
+++ lesson10.py
@@ -0,0 +1,12 @@
+def greet():
+    print('hello!')
+    print('welcome to Python!')
+
+def add(a, b):
+    result = a + b
+    print('the sum is', result)
+
+greet()
+greet()
+add(5, 3)
+add(10, 20)
