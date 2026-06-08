cf1132db9458fe5e24d564e86a4f15203ef6e85c
dhruva666 <dhruvahm2009@gmail.com>
2026-06-08 22:38:42 +0530

    lesson 11 dictionaries

---
+++ lesson11.py
@@ -0,0 +1,16 @@
+student = {
+    "name": "Dhruva",
+    "age": 17,
+    "grade": "12th",
+    "hobby": "anime"
+}
+
+print(student)
+print(student["name"])
+print(student["age"])
+
+student["city"] = "Channapatna"
+print(student)
+
+student["age"] = 18
+print(student["age"])
